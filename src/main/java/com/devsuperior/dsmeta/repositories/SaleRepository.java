3c40b64a72c1805b5ab51e80aeaa02ab8acc1a4c
Marcio Santos <santosu01@gmail.com>
2025-06-20 21:34:36 -0300

    feat: add get report and get summary

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,28 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.projections.SaleSummaryProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleReportDTO(obj.id, obj.date, obj.amount, obj.seller.name) " +
+            "FROM Sale obj " +
+            "WHERE obj.date BETWEEN :minDate AND :maxDate " +
+            "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :sellerName, '%'))")
+    Page<SaleReportDTO> getReport(LocalDate minDate, LocalDate maxDate, String sellerName, Pageable pageable);
+
+    @Query(nativeQuery = true, value = "SELECT tb_seller.name, SUM(tb_sales.amount) AS total " +
+            "FROM tb_sales " +
+            "INNER JOIN tb_seller ON tb_sales.seller_id = tb_seller.id " +
+            "WHERE tb_sales.date BETWEEN :minDate AND :maxDate " +
+            "GROUP BY tb_seller.name")
+    List<SaleSummaryProjection> getSummary(LocalDate minDate, LocalDate maxDate);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,13 +1,22 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.Optional;
-
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.projections.SaleSummaryProjection;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.devsuperior.dsmeta.dto.SaleMinDTO;
-import com.devsuperior.dsmeta.entities.Sale;
-import com.devsuperior.dsmeta.repositories.SaleRepository;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class SaleService {
@@ -19,5 +28,38 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleReportDTO> getReport(Pageable pageable, String minDate, String maxDate, String sellerName) {
+		LocalDate endDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate startDate = endDate.minusYears(1L);
+
+		if (minDate != null && !minDate.isEmpty()) {
+			startDate = LocalDate.parse(minDate);
+		}
+
+		if (maxDate != null && !maxDate.isEmpty()) {
+			endDate = LocalDate.parse(maxDate);
+		}
+
+
+        return repository.getReport(startDate, endDate, sellerName, pageable);
+	}
+
+	public List<SaleSummaryDTO> getSummary(String minDate, String maxDate) {
+		LocalDate endDate = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate startDate = endDate.minusYears(1L);
+
+		if (minDate != null && !minDate.isEmpty()) {
+			startDate = LocalDate.parse(minDate);
+		}
+
+		if (maxDate != null && !maxDate.isEmpty()) {
+			endDate = LocalDate.parse(maxDate);
+		}
+
+		List<SaleSummaryProjection> list = repository.getSummary(startDate, endDate);
+
+		return list.stream().map(SaleSummaryDTO::new).collect(Collectors.toList());
 	}
 }
